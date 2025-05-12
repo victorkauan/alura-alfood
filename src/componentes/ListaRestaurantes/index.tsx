657d70f7f8613e86daf3ab0ded30647c192cb8e4
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-11 23:16:54 -0300

    :sparkles: feat: Implement incremental pagination in restaurant list

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,23 +3,38 @@ import axios from 'axios';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import type { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [nextPageUrl, setNextPageUrl] = useState<string>('')
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(response => {
         setRestaurantes(response.data.results)
+        setNextPageUrl(response.data.next)
       })
       .catch(error => {
         console.log(error)
       })
   }, [])
 
+  const seeMore = () => {
+    axios.get<IPaginacao<IRestaurante>>(nextPageUrl)
+      .then(response => {
+        setRestaurantes([...restaurantes, ...response.data.results])
+        setNextPageUrl(response.data.next)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {nextPageUrl && <button onClick={seeMore}>Ver mais</button>}
   </section>)
 }
 
