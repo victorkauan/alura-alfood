eeed6459abbb44958f7e52028b453930e6d150e5
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-20 16:24:47 -0300

    :sparkles: feat: Implement admin base layout

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -5,45 +5,45 @@ import http from "../../../http"
 import type  IRestaurante from "../../../interfaces/IRestaurante"
 
 const FormularioRestaurante = () => {
-  const params = useParams()
+    const params = useParams()
 
-  const [name, setName] = useState<string>('')
+    const [name, setName] = useState<string>('')
 
-  useEffect(() => {
+    useEffect(() => {
+        if (params.id) {
+            http.get<IRestaurante>(`restaurantes/${params.id}/`)
+                .then(response => setName(response.data.nome))
+        }
+    }, [params.id])
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+
       if (params.id) {
-          http.get<IRestaurante>(`restaurantes/${params.id}/`)
-              .then(response => setName(response.data.nome))
+          http.patch(`restaurantes/${params.id}/`, { nome: name })
+              .then(() => alert('Restaurante atualizado com sucesso.'))
+      } else {
+          http.post('restaurantes/', { nome: name })
+              .then(() => alert('Restaurante cadastrado com sucesso.'))
       }
-  }, [params.id])
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    if (params.id) {
-        http.patch(`restaurantes/${params.id}/`, { nome: name })
-            .then(() => alert('Restaurante atualizado com sucesso.'))
-    } else {
-        http.post('restaurantes/', { nome: name })
-            .then(() => alert('Restaurante cadastrado com sucesso.'))
     }
-  }
 
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
-        <Box component="form" onSubmit={handleSubmit}>
-            <TextField
-              required
-              label="Nome do restaurante"
-              variant="standard"
-              value={name}
-              onChange={event => setName(event.target.value)}
-              fullWidth
-            />
-            <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
+    return (
+        <Box sx={{ display: 'flex', flexGrow: 1, flexDirection: 'column', alignItems: 'center' }}>
+            <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+            <Box component="form" sx={{ width: '100%' }} onSubmit={handleSubmit}>
+                <TextField
+                    required
+                    label="Nome do restaurante"
+                    variant="standard"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    fullWidth
+                />
+                <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
+            </Box>
         </Box>
-    </Box>
-  )
+    )
 }
 
 export default FormularioRestaurante
