b78a998dccf33f7d3c2a9e9809bd729b76adf9a6
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-02 16:47:47 -0300

    :sparkles: feat: Create admin dish create form

---
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -0,0 +1,116 @@
+import { type ChangeEvent, useEffect, useState, type FormEvent } from "react"
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material"
+import http from "../../../http"
+import type ITag from "../../../interfaces/ITag"
+import type IRestaurante from "../../../interfaces/IRestaurante"
+
+const FormularioPrato = () => {
+    const [name, setName] = useState<string>('')
+    const [description, setDescription] = useState<string>('')
+    const [tag, setTag] = useState<string>('')
+    const [restaurant, setRestaurant] = useState<string>('')
+    const [image, setImage] = useState<File | null>(null)
+
+    const [tags, setTags] = useState<ITag[]>([])
+    const [restaurants, setRestaurants] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        http.get<{ tags: ITag[] }>('tags/', )
+            .then(response => setTags(response.data.tags))
+
+        http.get<IRestaurante[]>('restaurantes/', )
+            .then(response => setRestaurants(response.data))
+    }, [])
+
+    const selectFile = (event: ChangeEvent<HTMLInputElement>) => {
+        if (!event.target.files?.length) {
+            return
+        }
+
+        setImage(event.target.files[0])
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const formData = new FormData()
+
+        formData.append('nome', name)
+        formData.append('descricao', description)
+        formData.append('tag', tag)
+        formData.append('restaurante', restaurant)
+
+        if (image) {
+            formData.append('imagem', image)
+        }
+
+        http.request({
+            url: 'pratos/',
+            method: 'POST',
+            headers: { 'Content-Type': 'multipart/form-data' },
+            data: formData
+        })
+            .then(() => {
+                setName('')
+                setDescription('')
+                setTag('')
+                setRestaurant('')
+                setImage(null)
+
+                alert('Prato cadastrado com sucesso!')
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <Box sx={{ display: 'flex', flexGrow: 1, flexDirection: 'column', alignItems: 'center' }}>
+            <Typography component="h1" variant="h6">Formulário de Pratos</Typography>
+            <Box component="form" sx={{ width: '100%' }} onSubmit={handleSubmit}>
+                <TextField
+                    required
+                    label="Nome do prato"
+                    variant="standard"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    margin="dense"
+                    fullWidth
+                />
+                <TextField
+                    required
+                    label="Descrição do prato"
+                    variant="standard"
+                    value={description}
+                    onChange={event => setDescription(event.target.value)}
+                    margin="dense"
+                    fullWidth
+                />
+                <FormControl margin="dense" fullWidth>
+                    <InputLabel id="tag-select">Tag</InputLabel>
+                    <Select labelId="tag-select" value={tag} onChange={event => setTag(event.target.value)}>
+                        {tags.map(tag => (
+                            <MenuItem key={tag.id} value={tag.value}>
+                                {tag.value}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <FormControl margin="dense" fullWidth>
+                    <InputLabel id="restaurant-select">Restaurante</InputLabel>
+                    <Select labelId="restaurant-select" value={restaurant} onChange={event => setRestaurant(event.target.value)}>
+                        {restaurants.map(restaurant => (
+                            <MenuItem key={restaurant.id} value={restaurant.id}>
+                                {restaurant.nome}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <input type="file" onChange={selectFile} />
+
+                <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
+            </Box>
+        </Box>
+    )
+}
+
+export default FormularioPrato
